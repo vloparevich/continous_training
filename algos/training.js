70d8ce28599219bd8d4f9ef78109f2af0a09cdcd
vloparevich <vadim.loparevich@gmail.com>
2021-12-11 13:10:56 -0500

    Implement arrayDiff

--- algos/training.js
+++ algos/training.js
@@ -112,3 +112,34 @@ describe('Data Reverse', function () {
     Test.assertDeepEquals(dataReverse(data3), data4);
   });
 });
+
+function arrayDiff(a, b) {
+  const result = [];
+  a.map((current) => {
+    if (!b.includes(current)) {
+      result.push(current);
+    }
+  });
+  return result;
+}
+
+describe('Sample tests', function () {
+  it('Should pass Sample tests', function () {
+    Test.assertDeepEquals(arrayDiff([], [4, 5]), [], 'a was [], b was [4,5]');
+    Test.assertDeepEquals(
+      arrayDiff([3, 4], [3]),
+      [4],
+      'a was [3,4], b was [3]'
+    );
+    Test.assertDeepEquals(
+      arrayDiff([1, 8, 2], []),
+      [1, 8, 2],
+      'a was [1,8,2], b was []'
+    );
+    Test.assertDeepEquals(
+      arrayDiff([1, 2, 3], [1, 2]),
+      [3],
+      'a was [1,2,3], b was [1,2]'
+    );
+  });
+});
