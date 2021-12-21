07c095be25466655c7e3deeabeb54f1e6fd93c99
vloparevich <54963273+vloparevich@users.noreply.github.com>
2021-12-21 12:03:33 -0800

    Update training.js

--- algos/training.js
+++ algos/training.js
@@ -138,3 +138,14 @@ describe('Sample tests', function () {
     );
   });
 });
+
+function twoSum(numbers, target) {
+  for(let i = 0; i < numbers.length - 1; i++){
+    for(let j = 1 + i; j < numbers.length; j++){
+      if(numbers[i]+numbers[j] === target){
+        return [i, j];
+      }
+    }
+  }
+ return [];
+}
