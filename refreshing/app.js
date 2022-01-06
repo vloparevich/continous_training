ab350d8a6f93d313df020aa70eabf075284b3940
vloparevich <vadim.loparevich@gmail.com>
2022-01-06 18:49:02 -0500

    Wrap up primitive node server

--- refreshing/app.js
+++ refreshing/app.js
@@ -1,35 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-// function rqListener(req, res) {
+console.log(routes.someText);
 
-// }
-
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //process.exit();
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></div></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.text', 'uuu');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<body><div>Hey Girla</div></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3001);
