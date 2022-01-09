2a98196d142e037247d5dbb328a4dcdbf70c169d
vloparevich <vadim.loparevich@gmail.com>
2022-01-09 13:10:19 -0500

    Switch to express

--- refreshing/app.js
+++ refreshing/app.js
@@ -1,8 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+  console.log('in the middleware');
+  next();
+});
 
-server.listen(3001);
+app.use((req, res, next) => {
+  console.log('in another middleware');
+  res.send('<h1>Hello From  Express</h1>');
+});
+
+app.listen(3001);
+// const server = http.createServer(app);
+// server.listen(3001);
