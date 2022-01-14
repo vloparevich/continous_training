b99d342b811400e89c8cc1867394677f975098e2
vloparevich <vadim.loparevich@gmail.com>
2022-01-13 19:10:34 -0500

    Add static access to public and add gitignore

--- refreshing/app.js
+++ refreshing/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log('in another middleware');
-  res.send('<h1>Hello From  Express</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3001);
-// const server = http.createServer(app);
-// server.listen(3001);
