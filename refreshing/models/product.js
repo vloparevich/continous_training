9c79ec447157bc448542b488c4c95629f1b9744d
vloparevich <vadim.loparevich@gmail.com>
2022-01-23 13:00:28 -0500

    Add temp data storage as .json

--- refreshing/models/product.js
+++ refreshing/models/product.js
@@ -34,7 +34,7 @@ module.exports = class Product {
 
     fs.readFile(p, (err, fileContent) => {
       if (err) {
-        cd([]);
+        cb([]);
       }
       cb(JSON.parse(fileContent));
     });
