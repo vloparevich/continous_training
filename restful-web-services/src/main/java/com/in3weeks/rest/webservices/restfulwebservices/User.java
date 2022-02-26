eb9d0c82e3c7d454b535392cbc9ecdb78fd323db
vloparevich <vadim.loparevich@gmail.com>
2022-02-26 11:00:23 -0500

    Init SpringBootApp

---
+++ restful-web-services/src/main/java/com/in3weeks/rest/webservices/restfulwebservices/User.java
@@ -0,0 +1,48 @@
+package com.in3weeks.rest.webservices.restfulwebservices;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Date birthDate;
+
+    public User(Integer id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+}
---
+++ restful-web-services/src/test/java/com/in3weeks/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in3weeks.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
