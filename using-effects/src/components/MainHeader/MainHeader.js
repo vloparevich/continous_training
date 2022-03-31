66a49969324fe59f4b7b86db671a2ef79a6044de
vloparevich <vadim.loparevich@gmail.com>
2022-03-30 21:00:37 -0400

    Utilize useContext hook

--- using-effects/src/components/MainHeader/MainHeader.js
+++ using-effects/src/components/MainHeader/MainHeader.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
+import AuthContext from '../../context/auth-context';
 
 const MainHeader = (props) => {
   return (
-    <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
-    </header>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return (
+          <header className={classes['main-header']}>
+            <h1>A Typical Page</h1>
+            <Navigation isLoggedIn={ctx} />
+          </header>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 };
 
